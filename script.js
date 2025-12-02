2cf89d66c2b0340e91d34a9f4510e36ab2763793
Csukárdi Krisztián <swainer20000228@gmail.com>
2025-12-02 09:54:25 +0100

    map-, web upgrade

---
+++ script.js
@@ -0,0 +1,14 @@
+const thumbs = document.querySelectorAll('.thumb');
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+
+thumbs.forEach(img => {
+    img.addEventListener('click', () => {
+        lightboxImg.src = img.src;
+        lightbox.style.display = 'flex';
+    });
+});
+
+lightbox.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+});
